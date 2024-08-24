fcb333932f23fc0e49e4fbbb90679ded297a69d4
PierceLe <haichau.hvsg@gmail.com>
2024-08-24 21:05:57 +1000

    chore: fix key of FireBase

--- frontend/src/firebase/firebase.tsx
+++ frontend/src/firebase/firebase.tsx
@@ -2,13 +2,13 @@ import { initializeApp, FirebaseApp } from "firebase/app";
 import { getAuth, Auth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB56Vmlz9r8RAtcJqKEIBQVzXv7LBpNygI",
-  authDomain: "auth-test-1e3fc.firebaseapp.com",
-  projectId: "auth-test-1e3fc",
-  storageBucket: "auth-test-1e3fc.appspot.com",
-  messagingSenderId: "335692353598",
-  appId: "1:335692353598:web:9040c76db36d3b077fc16f",
-  measurementId: "G-40KX03X61L",
+  apiKey: "AIzaSyD0p2LWNfXfiMYrISoeG-GOIDdfgD8KZGg",
+  authDomain: "syncs-hacks.firebaseapp.com",
+  projectId: "syncs-hacks",
+  storageBucket: "syncs-hacks.appspot.com",
+  messagingSenderId: "532115676111",
+  appId: "1:532115676111:web:d79ec049f867646a3290ba",
+  measurementId: "G-RTELQMVDYP"
 };
 
 // Initialize Firebase
