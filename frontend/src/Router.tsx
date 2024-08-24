f5a66c8632703c2314cb02f3887fb208fd52119b
abyanmajid <abyan@abydyl.net>
2024-08-24 13:05:21 +1000

    added: parser pdf to text

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
+import { LearnPage } from './pages/Learn.page';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
   },
+  {
+    path: '/learn',
+    element: <LearnPage />
+  }
 ]);
 
 export function Router() {
