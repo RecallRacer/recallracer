ed82d2e012b6feb9f1491322060af1a746b1bffd
IceyVision <iceyvision01@gmail.com>
2024-08-24 13:21:38 +1000

    Refactor: Setting up home page

--- frontend/src/pages/Home.page.tsx
+++ frontend/src/pages/Home.page.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
-import { Center, Code, Stack, Text, Title } from '@mantine/core';
+import { Center, Code, Stack, Title } from '@mantine/core';
 import { useHealthCheck } from '@/hooks/useHealthCheck';
+
+import { Container, Text, Button, Group } from '@mantine/core';
+import { GithubIcon } from '@mantinex/dev-icons';
+import classes from '../HeroTitle.module.scss';
 
 export function HomePage() {
   const { healthCheck } = useHealthCheck();
@@ -15,22 +19,43 @@ export function HomePage() {
   }, []);
 
   return (
-    <>
-      <Center>
-        <Stack>
-          <Title>Hackathon Starter</Title>
-          <Text>
-            Get started by editing <Code>/pages</Code>
-          </Text>
-          <Title>Example of an API Call from Backend</Title>
-          <Text>
-            POST request sent to <Code>localhost:8080/health</Code>
-          </Text>
-          <Text>
-            Response from Backend: <Code>{health}</Code>
-          </Text>
-        </Stack>
-      </Center>
-    </>
+    <div className={classes.wrapper}>
+      <Container size={700} className={classes.inner}>
+        <h1 className={classes.title}>
+          A{' '}
+          <Text component="span" variant="gradient" gradient={{ from: 'blue', to: 'cyan' }} inherit>
+            fully featured
+          </Text>{' '}
+          React components and hooks library
+        </h1>
+
+        <Text className={classes.description} color="dimmed">
+          Build fully functional accessible web applications with ease – Mantine includes more than
+          100 customizable components and hooks to cover you in any situation
+        </Text>
+
+        <Group className={classes.controls}>
+          <Button
+            size="xl"
+            className={classes.control}
+            variant="gradient"
+            gradient={{ from: 'blue', to: 'cyan' }}
+          >
+            Get started
+          </Button>
+
+          <Button
+            component="a"
+            href="https://github.com/mantinedev/mantine"
+            size="xl"
+            variant="default"
+            className={classes.control}
+            leftSection={<GithubIcon size={20} />}
+          >
+            GitHub
+          </Button>
+        </Group>
+      </Container>
+    </div>
   );
 }
